fa4bf7fd89ea7abb585b5db33861791d4b8e1567
rizwan230698 <rizwananwar230698@gmail.com>
2020-04-14 20:02:27 +0530

    Updates

---
+++ precache-manifest.b9b64f91a9fe8bf7de4ed06dee8c6019.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e14d159bc3f757eec1560e13b177f2ae",
+    "url": "/chat-room/index.html"
+  },
+  {
+    "revision": "1166505cd4d8e9461c31",
+    "url": "/chat-room/static/css/main.987cb0de.chunk.css"
+  },
+  {
+    "revision": "dc603c8e3b9030d55700",
+    "url": "/chat-room/static/js/2.e753b438.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/chat-room/static/js/2.e753b438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1166505cd4d8e9461c31",
+    "url": "/chat-room/static/js/main.01c214f7.chunk.js"
+  },
+  {
+    "revision": "354429addc4c48569317",
+    "url": "/chat-room/static/js/runtime-main.d9b08a44.js"
+  }
+]);
